a67b2524d213812942b43f696c29ad54189a80e8
Oleg <oleg17.k@gmail.com>
2023-10-08 14:54:04 +0300

    left sidebar done

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import React from "react";
 
 const GlobalSearch = () => {
+  const searchHandler = () => {};
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -17,6 +19,7 @@ const GlobalSearch = () => {
           type="text"
           placeholder="Search globally"
           value=""
+          onChange={searchHandler}
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
